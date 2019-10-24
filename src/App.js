11f8827e1280af24fccd3f0f15c400e3c6965334
Simon TSCHAN <tschan.omis@gmail.com>
2019-10-24 10:54:52 +0200

    first character

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import axios from 'axios';
 import DisplaySimpsons from './components/DisplaySimpsons';
 import './App.css';
 
+const first = 
+  {
+  "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+  "character": "Apu Nahasapeemapetilon",
+  "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+  "characterDirection": "Left"
+  }
+
+console.log(first);
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      simpson: "",
+      simpson: first,
     };
     this.getSimpsons = this.getSimpsons.bind(this);
   }
